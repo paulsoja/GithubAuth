2104ef8c552d3947b39aaaf25994d97db388ab15
paulsoja <p-shadow@ukr.net>
2020-09-01 20:48:06 +0300

    Initial commit

---
+++ app/src/main/java/com/paulsoia/githubauth/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.paulsoia.githubauth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
